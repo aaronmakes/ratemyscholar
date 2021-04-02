f1821e6e84d102325aa93bf67f7144685fb2629c
aaronmakes <59892654+aaronmakes@users.noreply.github.com>
2021-04-02 19:26:22 -0400

    changed root name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const app = express();
-app.use(express.static('public'));
+app.use(express.static('ratemyscholar'));
 app.get('/', (req, res) => {
-     res.sendFile('index.html', {root: __dirname + '/public/'});
+     res.sendFile('index.html', {root: __dirname + '/ratemyscholar/'});
 });
 app.listen(process.env.PORT || 5000);
